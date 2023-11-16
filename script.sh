4f90e77ed57cb5073cc9986b7235d0d9cba55861
André Vinícius dos Santos <32529232+andrevinicius201@users.noreply.github.com>
2023-11-16 17:17:29 -0300

    Add files via upload

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone https://github.com/aws-samples/serverless-samples.git
+git clone https://github.com/andrevinicius201/soap-pattern.git
 sudo yum -y install gcc openssl-devel bzip2-devel libffi-devel
 wget https://www.python.org/ftp/python/3.9.16/Python-3.9.16.tgz
 sudo tar xzf Python-3.9.16.tgz
@@ -7,6 +7,6 @@ cd Python-3.9.16
 sudo ./configure --enable-optimizations
 sudo make altinstall
 cd ..
-# cd serverless-samples/
-# sam build && sam deploy
-# y
+cd soap-pattern/
+sam build
+sam deploy --no-confirm-changeset
