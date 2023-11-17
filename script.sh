8a8fe43fdcd393e0de9ebe0f6a57026fa1f51763
André Vinícius dos Santos <32529232+andrevinicius201@users.noreply.github.com>
2023-11-17 09:31:01 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-wget https://github.com/andrevinicius201/soap-serverless/archive/main.zip
-unzip main.zip
+echo Please enter the REST API endpoint. Optionally, you can leave it blank and set it later in the xslt-transformer lambda function
+read varname
+
+if [ -z "$varname" ]; then
+  echo "No endpoint was set at this moment"
+else
+  echo The solution will be able to invoke this REST endpoint: $varname
+fi
+git clone https://github.com/andrevinicius201/soap-serverless.git
 sudo yum -y install gcc openssl-devel bzip2-devel libffi-devel
 wget https://www.python.org/ftp/python/3.9.16/Python-3.9.16.tgz
 sudo tar xzf Python-3.9.16.tgz
@@ -8,6 +15,14 @@ cd Python-3.9.16
 sudo ./configure --enable-optimizations
 sudo make altinstall
 cd ..
-cd soap-serverless-main/
-sam build
-sam deploy --no-confirm-changeset
+cd soap-serverless/
+sam build && sam deploy
+if [ -z "$varname" ]; then
+  echo "Setup is complete"
+else
+    aws lambda update-function-configuration \
+        --function-name  xslt-transformer \
+        --environment Variables={KEY1=$varname} \
+        > output.txt
+  echo Setup complete
+fi
