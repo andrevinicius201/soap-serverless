27fb9559ccd5464c2e7cf0479e752903faa274fe
André Vinícius dos Santos <32529232+andrevinicius201@users.noreply.github.com>
2023-11-22 11:34:31 -0300

    Add files via upload

--- script.sh
+++ script.sh
@@ -8,7 +8,7 @@ else
   echo The solution will be able to invoke this REST endpoint: $endpoint
 fi
 wget https://github.com/andrevinicius201/soap-serverless/archive/main.zip
-unzip main.zip
+unzip soap-serverless-main.zip
 # sudo yum -y install gcc openssl-devel bzip2-devel libffi-devel
 # wget https://www.python.org/ftp/python/3.9.16/Python-3.9.16.tgz
 # sudo tar xzf Python-3.9.16.tgz
@@ -16,11 +16,16 @@ unzip main.zip
 # sudo ./configure --enable-optimizations
 # sudo make altinstall
 # cd ..
-cd cd soap-serverless-main/
-cd function/
+cd soap-serverless-main/function/
 zip lambda-code.zip app.py
-aws s3 cp ./lambda-code.zip s3://soap-transformer-$ACC_ID/
-aws cloudformation create-stack --stack-name soap-pattern-stack --template-body file://cloudformation-template.json
+aws s3api create-bucket --bucket assets-soap-pattern-$ACC_ID
+aws s3 cp ./lambda-code.zip s3://assets-soap-pattern-$ACC_ID/
+cd ..
+aws s3 cp ./dependencies.zip s3://assets-soap-pattern-$ACC_ID/
+aws s3 cp ./openapi.yaml s3://assets-soap-pattern-$ACC_ID/
+aws cloudformation create-stack --stack-name soap-pattern-stack --template-body file://cloudformation-template.json --capabilities CAPABILITY_IAM
+aws s3 cp ./xslt s3://soap-transformer-$ACC_ID/xslt --recursive
+cd ..
 # sam build
 # sam deploy --no-confirm-changeset
 # if [ -z "$endpoint" ]; then
@@ -43,4 +48,4 @@ aws cloudformation create-stack --stack-name soap-pattern-stack --template-body 
 #         --function-name xslt-transformer \
 #         --environment "{\"Variables\": $updated_env_vars}"
 # fi
-aws s3 cp ./xslt s3://soap-transformer-$ACC_ID/xslt --recursive
+
